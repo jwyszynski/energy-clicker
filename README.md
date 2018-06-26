65796b64a4d1ec7659ffc633b786e4db8c017286
Jakub Wyszyński <33234553+jwyszynski@users.noreply.github.com>
2018-06-26 02:05:59 +0200

    Energy-clicker Wersja 0.2

---
+++ README.md
@@ -0,0 +1,20 @@
+# ENERGY CLICKER
+
+Prosta gra napisana w JAVASCRIPT HTML i CSS
+
+## DEMO:
+
+Niedługo będzie dostępne na mojej stronie: [http://jwyszynski.pl/](http://jwyszynski.pl/)
+
+## O czym to?
+
+Wcielamy się w właściciela elektrowni, który buduje swoje energetyczne imperium od zera mając jedynie ręczny generator.
+
+## TODO
+
+Ulepszenia:
+- turbina wiatrowa
+- panele słoneczne
+- generator na węgiel
+- zapora
+- reaktor atomowy
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//
+// ZMIENNE:
+//
 let energia = 0;
 let lepszaKorba = 0;
 let lepszyGen = 0;
@@ -5,20 +8,17 @@ let malyGeneratorWodny = 0;
 let malyGeneratorWiatrowy = 0;
 let perClick = 1;
 let perSec = 0;
+// Koszty:
+let kosztKorby = 10;
+let kosztGen = 100;
+let kosztMalyGenWod = 10;
+let kosztMalyGenWiat = 100;
 
-let licznikEnergii = document.querySelector('.energia');
-let iloscKorb = document.querySelector('#iloscKorb');
-let iloscGen = document.querySelector('#iloscGen');
-let iloscGenWod = document.querySelector('#iloscGenWod');
-let iloscGenWiat = document.querySelector('#iloscGenWiat');
-
-
-let iloscPerClick = document.querySelector('#perClick');
 
 function klik() {
 
   energia += perClick;
-  console.log(`energia: ${energia}`);
+  console.log(`Obecna energia: ${energia}`);
 
 }
 
@@ -28,36 +28,108 @@ function addPerSec() {
 }
 
 function odswiez() {
-  licznikEnergii.innerHTML = energia;
-  iloscKorb.innerHTML = lepszaKorba;
-  iloscGen.innerHTML = lepszyGen;
-  iloscGenWod.innerHTML = malyGeneratorWodny;
-  iloscGenWiat.innerHTML = malyGeneratorWiatrowy;
-  iloscPerClick.innerHTML = perClick;
-  perClick = 1+lepszaKorba+lepszyGen
+  //ODSWIEŻANIE WARTOŚCI NA INTERFEJSIE GRAFICZNYM
+  document.querySelector('.energia').innerHTML = energia;
+  document.querySelector('#iloscKorb').innerHTML = lepszaKorba;
+  document.querySelector('#iloscGen').innerHTML = lepszyGen;
+  document.querySelector('#iloscGenWod').innerHTML = malyGeneratorWodny;
+  document.querySelector('#iloscGenWiat').innerHTML = malyGeneratorWiatrowy;
+  document.querySelector('#perClick').innerHTML = perClick;
+  document.querySelector('#iloscKosztKorby').innerHTML = kosztKorby;
+  document.querySelector('#kosztGen').innerHTML = kosztGen;
+  document.querySelector('#kosztMalyGenWod').innerHTML = kosztMalyGenWod;
+  document.querySelector('#kosztMalyGenWiat').innerHTML = kosztMalyGenWiat;
+  document.querySelector('#perSec').innerHTML = perSec;
+  perClick = 1+lepszaKorba+(lepszyGen*10);
 
+}
+//
+// ULEPSZENIA
+//
+function ileEnergii(_potrzeba) {
+  //Sprawdza czy jest wystarczająco energii
+  if (energia >= _potrzeba) {
+    console.log("Yo " + _potrzeba);
+    return true;
+  } else {
+    infobox(`Potrzebujesz ${_potrzeba} energii`);
+    console.log("nie ma tyle energii");
+    return false;
+  }
 }
 
 function kupLepszaKorba() {
-  lepszaKorba += 1;
-  console.log(`Lepsza Korba: ${lepszaKorba}`);
+  //Kupowanie Korby
+  if (ileEnergii(kosztKorby)) {
+    energia -= kosztKorby;
+    lepszaKorba += 1;
+    console.log(`Lepsza Korba: ${lepszaKorba}`);
+    kosztKorby = (lepszaKorba+1)*10;
+  }
 }
 
 function kupLepszyGen() {
-  lepszyGen += 1;
-  console.log(`Lepszy generator: ${lepszyGen}`);
+  //Kupowanie Generatora
+  if (ileEnergii(kosztGen)) {
+    energia -= kosztGen;
+    lepszyGen += 1;
+    console.log(`Lepszy Gen: ${lepszyGen}`);
+    kosztGen = (lepszyGen+1)*100;
+  }
 }
 
 function kupMalyGenWodny() {
-  malyGeneratorWodny += 1;
-  console.log(`malyGeneratorWodny: ${malyGeneratorWodny}`);
+  //Kupowanie Elektrowni Wodnej
+  if (ileEnergii(kosztMalyGenWod)) {
+    energia -= kosztMalyGenWod;
+    malyGeneratorWodny += 1;
+    console.log(`Mały Generator Wodny: ${malyGeneratorWodny}`);
+    kosztMalyGenWod = (malyGeneratorWodny+1)*10;
+  }
 }
 
 function kupMalyGenWiatrowy() {
-  malyGeneratorWiatrowy += 1;
-  console.log(`malyGeneratorWiatrowy: ${malyGeneratorWiatrowy}`);
+  //Kupowanie Elektrowni Wiatrowej
+  if (ileEnergii(kosztMalyGenWiat)) {
+    energia -= kosztMalyGenWiat;
+    malyGeneratorWiatrowy += 1;
+    console.log(`Lepszy Gen: ${malyGeneratorWiatrowy}`);
+    kosztMalyGenWiat = (malyGeneratorWiatrowy+1)*100;
+  }
 }
 
 
+// WYWOŁYWANE FUNKCJE
+
 setInterval(odswiez, 100);
 setInterval(addPerSec, 1000);
+
+//INFOBOX - Wyświetla daną wiadomość w oknie
+let infoboxTimeout;
+function infobox(_info) {
+  if (infoboxTimeout!=undefined) clearTimeout(infoboxTimeout);
+  document.querySelector("#infobox").innerHTML = _info;
+  document.querySelector("#infobox").classList.add("infoboxblink");
+  infoboxTimeout = setTimeout(function(){
+    document.querySelector("#infobox").classList.remove("infoboxblink");
+  }, 1500);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//END
